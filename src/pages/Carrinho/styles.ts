bb0644568e06e54f31cb9f95aac7b40b9d3acb9a
CLSCaio <caiodelimack@hotmail.com>
2022-01-14 19:34:33 -0300

    frontEnd cartAplication

---
+++ src/pages/Carrinho/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  margin-top: 30px;
+  height: 50px;
+  width: 150px;
+  text-align: center;
+
+  background-color: red;
+  color: black;
+  border-color: var(--second-color)
+
+  cursor: pointer;
+`
